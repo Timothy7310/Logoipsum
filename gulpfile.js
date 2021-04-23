7bbf0cdd84ff5efc610fb0b69283b362828ff14b
Timothy <penlist127@gmail.com>
2021-04-23 14:53:48 +0300

    added focus-visible at gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ function cleanDist(){
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/focus-visible/dist/focus-visible.min.js',
         'app/js/swiper-bundle.min.js',
         'app/js/main.js'
     ])
